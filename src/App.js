3b6bdce5e14b705d01bd08143928e33fb4fb844b
Chris <ctela@uwo.ca>
2025-09-09 23:49:35 -0400

    removed analyitics, need react 18

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
-import { Analytics } from "@vercel/analytics/react"
-
 
 function App() {
   return (
     <div>
       <Main />
-      <Analytics />
     </div>
   );
 }
